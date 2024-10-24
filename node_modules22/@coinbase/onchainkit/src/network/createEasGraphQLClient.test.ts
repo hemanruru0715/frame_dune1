3143f558c302ee6eac2323453ccdef92be99ccbe
hemanruru0715 <hemanruru@gmail.com>
2024-10-24 21:32:58 +0900

    노드모듈 제거

--- node_modules22/@coinbase/onchainkit/src/network/createEasGraphQLClient.test.ts
+++
@@ -1,9 +0,0 @@
-import { base } from 'viem/chains';
-import { createEasGraphQLClient } from './createEasGraphQLClient';
-
-describe('createEasGraphQLClient', () => {
-  it('should return a easGraphqlClient', () => {
-    const easGraphqlClient = createEasGraphQLClient(base);
-    expect(easGraphqlClient.requestConfig).toEqual({});
-  });
-});
